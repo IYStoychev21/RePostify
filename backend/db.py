e72f63dab340289d70a221e0806e4d4a18a6f6ee
Ivaylo Bachvarov <ISBachvarov21@codingburgas.bg>
2023-11-21 20:28:32 +0200

    Add init_db.py

--- backend/db.py
+++ backend/db.py
@@ -5,7 +5,3 @@ import os
 load_dotenv()
 conn = psycopg2.connect(host="postgres", port="5432", dbname="postgres", password=os.getenv("PASSWORD"), user="postgres")
 cur = conn.cursor()
-
-# GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
-# GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
-# GOOGLE_REDIRECT_URI = os.getenv("GOOGLE_REDIRECT_URI")
