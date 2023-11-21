02f455a3b71a54c71129e4ee23926de78772ca21
IYStoychev21 <IYStoychev21@codingburgas.bg>
2023-11-21 09:57:11 +0200

    Add way to push posts with images

--- frontend/src/components/NewPost.jsx
+++ frontend/src/components/NewPost.jsx
@@ -5,10 +5,12 @@ import instagramIcon from '/icon/instagram-icon.svg'
 import twitterIcon from '/icon/x-icon.svg'
 import { useState } from 'react'
 
-export default function NewPost() {
+export default function NewPost(props) {
     let fileInput = null
     let [image, setImage] = useState(null)
     let [preview, setPreview] = useState(null)
+    
+    let [inputText, setInputText] = useState("")
 
     const uploadImage = (event) => {
         setImage(() => event.target.files[0]) 
@@ -20,10 +22,27 @@ export default function NewPost() {
         image.readAsDataURL(event.target.files[0])
     }
 
+    const captureInput = (event) => {
+       setInputText(event.target.value)
+    }
+
+    const submitPost = () => {
+        let post = {
+            text: inputText,
+            image: image,
+        }
+
+        setInputText("")
+        setImage(null)
+        setPreview(null)
+
+        props.setpost((prev) => [...prev, post].reverse())
+    }
+
     return (
         <>
             <div className="flex flex-col w-fit items-center gap-2 mb-12">
-                <textarea className="rounded-lg p-5 resize-none text-lg bg-secondary-200" placeholder="Enter Text" name="newpost" id="newpost" cols="50" rows="6"></textarea>
+                <textarea onChange={captureInput} value={inputText} className="rounded-lg p-5 resize-none text-lg bg-secondary-200" placeholder="Enter Text" name="newpost" id="newpost" cols="50" rows="6"></textarea>
 
                 <div className="flex relative w-full">
                     <div className="flex">
@@ -50,9 +69,9 @@ export default function NewPost() {
                         </div>
                     </div>
                 </div>
-                <img src={preview} className='max-w-md rounded-2xl'/>
+                <img src={preview} className='max-w-md rounded-xl'/>
 
-                <button className="p-3 w-3/4 text-xl text-text-50 hover:scale-105 active:scale-100 duration-75 hover:bg-secondary-700 bg-accent-600 rounded-xl" type="submit">PUSH FOR REVIEW</button>
+                <button onClick={submitPost} className="p-3 w-3/4 text-xl text-text-50 hover:scale-105 active:scale-100 duration-75 hover:bg-secondary-700 bg-accent-600 rounded-xl" type="submit">PUSH FOR REVIEW</button>
             </div>
         </>
     )
--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
+
 export default function Post(props) {
+    let [image, setImage] = useState(null)
+
+    if(props.image != null)
+    {
+        const imageReader = new FileReader;
+        imageReader.onload = () => {
+            setImage(imageReader.result)
+        }
+        imageReader.readAsDataURL(props.image)
+    }
+
     return (
         <>
-            <div className="bg-background-300 rounded-xl p-5">
+            <div className="rounded-xl p-5 bg-primary-200">
                 <div className="flex items-center gap-2"> 
                     <img className="rounded-full hover:scale-105 cursor-pointer duration-150 active:scale-100 select-none bg-accent-500" src="https://api.dicebear.com/7.x/miniavs/svg?flip=true" height="32" width="32" alt="" />
                     <h2>Joe Doe</h2>
                 </div>
 
-               <p>{props.text}</p> 
+                <p className="mt-2">{props.text}</p> 
+                {image != null && <div className="flex justify-center mt-4">
+                    <img src={image} className='max-w-md rounded-xl text-center'/>
+                </div>}
             </div>
         </>
     );
