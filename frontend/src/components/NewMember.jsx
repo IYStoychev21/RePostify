0c65447908bef8bddb1d5b7b594c525e28a74e8c
IYStoychev21 <IYStoychev21@codingburgas.bg>
2023-11-22 21:58:26 +0200

    Add way to add members to an organization

---
+++ frontend/src/components/NewMember.jsx
@@ -0,0 +1,17 @@
+
+export default function NewMember(props) {
+    return (
+        <>
+            <div className="w-full flex gap-3">
+                <input onChange={props.getEmail} placeholder="Enter users email E.g. john@gmail.com" type="email" pattern=".+@gmail\.com" className="w-full p-2 rounded-md" required/>     
+
+                <select onChange={props.getRole} className="p-2 rounded-md" name="role" id="role">
+                    <option value="user">User</option>
+                    <option value="PR">PR</option>
+                </select>
+
+                <div onClick={props.removeMember} className="rounded-md w-12 hover:scale-105 active:scale-100 hover:bg-[#6e2525] h-full bg-[#b43a3a] duration-100 cursor-pointer"></div>
+            </div> 
+        </>
+    )
+}
--- frontend/src/routes/NewOrganization.jsx
+++ frontend/src/routes/NewOrganization.jsx
@@ -1,30 +1,58 @@
 import axios from "axios"
 import { useState, useEffect } from "react"
+import NewMember from "../components/NewMember"
 
-export default function NewOrganization() {
-    let name = null
-    let [userData, setUserData] = useState(null)
-
-    const getNameOnChange = (event) => {
-        name = event.target.value
+class Member {
+    constructor() {
+        this.email = null
+        this.role = "user"
     }
 
+    gatherEmail(event) {
+        this.email = event.target.value
+    }
+
+    gatherRole(event) {
+        this.role = event.target.value
+    }
+}
+
+export default function NewOrganization() {
+    let [name, setName] = useState(null)
+    let [userData, setUserData] = useState(null)
+    let [membersEntries, setMembersEntries] = useState([])
+    
     useEffect(() => { 
         axios.get('http://localhost:8000/user', { withCredentials: true })
-        .then((res) => {
+        .then((res) => { 
             setUserData(res.data)
         })
     }, [])
+    
+    const getNameOnChange = (event) => {
+        setName(event.target.value)
+    }
 
-    const createOrganization = () => {
+    const createOrganization = (event) => {
+        event.preventDefault()
+
         let data = {
-            id: 2,
             name: name,
-            members: [userData.email],
+            members: membersEntries,
             owner: userData.email
         }
 
         console.log(data)
+    }
+
+    const addNewMember = () => {
+        setMembersEntries([...membersEntries, new Member()])
+    }
+
+    const removeMember = (index) => {
+        let temp = [...membersEntries]
+        temp.splice(index, 1)
+        setMembersEntries(temp)
     }
 
     return (
@@ -33,10 +61,21 @@ export default function NewOrganization() {
                 <h1 className="text-3xl font-light m-4">NEW ORGANIZATION</h1>
 
                 <div className="flex flex-col h-[80vh] justify-center items-center">
-                    <div className="flex flex-col w-1/3 p-8 gap-5 bg-background-200 rounded-md">
-                        <input type="text" onChange={getNameOnChange} className="w-full p-2 rounded-md" placeholder="ORGANIZATION NAME" name="name" id="name" /> 
-                        <button onClick={createOrganization} className="p-3 bg-secondary-600 duration-100 hover:scale-105 hover:bg-secondary-700 active:scale-100 rounded-md">CREATE NEW ORGANIZATION</button>
-                    </div>
+                    <form className="flex flex-col w-1/3 p-8 gap-5 bg-background-200 rounded-md" onSubmit={createOrganization} method="post">
+
+                        <input type="text" onChange={getNameOnChange} className="w-full p-2 rounded-md" placeholder="ORGANIZATION NAME" name="name" id="name" required/> 
+                        <div className="flex flex-col gap-4">
+                            {
+                                membersEntries.map((member, index) => {
+                                    return <NewMember key={index} getEmail={(event) => member.gatherEmail(event)} getRole={(event) => member.gatherRole(event)} removeMember={() => removeMember(index)}/>
+                                })
+                            }
+                        </div>
+
+                        <button onClick={addNewMember} className="rounded-md bg-accent-200 p-2 hover:scale-105 hover:bg-accent-300 active:scale-100 duration-100"> Add New Member </button>
+                        <button type="submit" className="p-3 bg-secondary-600 duration-100 hover:scale-105 hover:bg-secondary-700 active:scale-100 rounded-md">CREATE NEW ORGANIZATION</button>
+
+                    </form>
                 </div>
             </div>
         </>
