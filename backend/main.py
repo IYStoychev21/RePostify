42b53bb67fedc4f78ac773eacc32468f415514a7
Ivaylo Bachvarov <ISBachvarov21@codingburgas.bg>
2023-11-28 21:37:53 +0200

    Fix database

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from modules import users, posts, organisations, authentication
+from modules import users, posts, organisations, authentication, init_db, drop
 from starlette.middleware.sessions import SessionMiddleware
 import os
 from fastapi.security import OAuth2PasswordBearer
@@ -29,3 +29,6 @@ app.router.include_router(users.router)
 app.router.include_router(posts.router)
 app.router.include_router(organisations.router)
 app.router.include_router(authentication.router)
+
+drop.drop_db()
+init_db.init_db()
